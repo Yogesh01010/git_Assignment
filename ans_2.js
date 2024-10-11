ff2d85093ca776a6943b323607182cab747c3b41
Yogesh01010 <112406803+Yogesh01010@users.noreply.github.com>
2024-10-11 22:46:43 +0530

    Update ans_2.js

--- ans_2.js
+++ ans_2.js
@@ -1,11 +1,1 @@
-function forEachCustom(arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i], i, arr);
-    }
-}
 
-const numbers = [10, 20, 30];
-
-forEachCustom(numbers, (element, index, array) => {
-    console.log(`Element: ${element}, Index: ${index}, Array: ${array}`);
-});
